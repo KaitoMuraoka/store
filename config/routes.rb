2ac5b50e87c6d4b967131a6389e081ff223c0544
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-05-31 14:15:11 +0900

    商品一覧と詳細を表示

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # get "/products", to: "products#index"
+  # post "/products", to: "products#create"
+  resources :products
+
+  # /blog/hello-world でマッチさせる
+  get "/blog/:title", to: "blog#show"
 end
