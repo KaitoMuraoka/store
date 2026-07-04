efa01f16211c9b49f0c3ed6c0de4c0b8ae3f6096
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-07-04 11:32:23 +0900

    Action Mailer とメール通知

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,10 @@ Rails.application.routes.draw do
   put "/products/:id", to: "products#update"
 
   delete "/products/:id", to: "products#destroy"
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
 
   # /blog/hello-world でマッチさせる
   get "/blog/:title", to: "blog#show"
