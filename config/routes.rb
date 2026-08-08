f1771c966c38226e99555ef4966022b916afc3be
KaitoMuraoka <70003919+KaitoMuraoka@users.noreply.github.com>
2026-08-08 23:33:57 +0900

    実装ミスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -13,23 +13,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "products#index"
-  get "/products", to: "products#index"
 
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
-
-  get "/products/:id", to: "products#show"
-
-  get "/products/:id/edit", to: "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
-
-  delete "/products/:id", to: "products#destroy"
   resources :products do
     resources :subscribers, only: [ :create ]
   end
   resource :unsubscribe, only: [ :show ]
-
-  # /blog/hello-world でマッチさせる
-  get "/blog/:title", to: "blog#show"
 end
